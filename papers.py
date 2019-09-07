3c6d8d75182d23ed4caac203ba19e5cb9dfd1555
Subrata Sarker <subrata_sarker@yahoo.com>
2019-09-07 16:10:19 +0000

    Render papers as json file using Flask

--- papers.py
+++ papers.py
@@ -1,9 +1,27 @@
-from flask import Flask, escape, request
+import csv
+from os import path
+from flask import Flask, escape, json, jsonify, request
 
 app = Flask(__name__)
 
+folder = path.join(path.dirname(__file__), "data")
+
+def get_json_data(filename, start, length):
+    with open(filename) as infile:
+        data = json.load(infile)["data"]
+        return jsonify({"data": data[start : start + length]})
+
+
 @app.route('/')
-def hello():
+def home():
     name = request.args.get("name", "World")
-    return f"Hello, {escape(name)}!"
+
+    if name == "dsscs" or name == "prscs":
+        start = int(request.args.get("start", 0))
+        length = int(request.args.get("length", 50))
+        filepath = path.join(folder, f"{name}_papers.json")
+        return get_json_data(filepath, start, length)
+    else:
+        return f"Hello, {escape(name)}! It works."
+
 
