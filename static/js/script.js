33a57a492603f7cd9c26ced22e745ecce54e940c
Subrata Sarker <subrata_sarker@yahoo.com>
2019-09-09 15:58:12 +0000

    Track DSSCs and PrSCs separately

--- static/js/script.js
+++ static/js/script.js
@@ -6,29 +6,43 @@
     }
     
     const displayStatus = () => {
-        const statusRowNum = localStorage.getItem("lastViewedRow");
-        const statusSolarType = localStorage.getItem("lastViewedSolar");
-        document.querySelector('.status__row-info').textContent = statusRowNum || "None";
-        document.querySelector('.status__solar-info').textContent = statusSolarType || "None";
+        const statusSolarType = localStorage.getItem("lastViewedSolar") || "None";
+        let statusRowNum = undefined;
+        
+        if (statusSolarType === 'dsscs') {
+            statusRowNum = localStorage.getItem("lastViewedDSSCs");
+        } else if (statusSolarType === 'prscs') {
+            statusRowNum = localStorage.getItem("lastViewedPrSCs");
+        }
+       
+        document.querySelector(".status__solar-info").textContent = statusSolarType;
+        document.querySelector(".status__row-info").textContent = statusRowNum || "None";
+
     };
     
     const updateStatus = event => {
         const target = event.target;
-        let rowNum = localStorage.getItem("lastViewedRow") || NaN;
-        let solarType = localStorage.getItem("lastViewedSolar") || '';
+        let solarType = localStorage.getItem("lastViewedSolar") || "";
         let query = "";
         
         if (target.nodeName === "TD" && target.className === "query") {
             const rowId = target.parentElement.id;
-            solarType = rowId.split("-")[0];
-            rowNum = parseInt(rowId.split("-")[1]) + 1;
+            const rowNum = parseInt(rowId.split("-")[1]) + 1;
             query = target.textContent;
+            solarType = rowId.split("-")[0];
+            
+             if (solarType === 'dsscs') {
+                localStorage.setItem("lastViewedDSSCs", rowNum);
+            } else if (solarType === 'prscs') {
+                localStorage.setItem("lastViewedPrSCs", rowNum);
+            }
+           
         } else if (target.className === "cells") {
             solarType = event.target.textContent.toLowerCase();
         }
 
-        localStorage.setItem("lastViewedRow", rowNum);
         localStorage.setItem("lastViewedSolar", solarType);
+        
         displayStatus();
         
         if (!!query) {
