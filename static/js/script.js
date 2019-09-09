c4c9e15b40bdbd5f437f77f168dfbc8a918da0e5
Subrata Sarker <subrata_sarker@yahoo.com>
2019-09-09 14:27:38 +0000

    Open link with google search query on click using javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,41 @@
 (() => {
     
+    const openBrowser = (query) => {
+        const url = "https://www.google.com/search?q=" + query;
+        window.open(url);
+    }
+    
     const displayStatus = () => {
         const statusRowNum = localStorage.getItem("lastViewedRow");
         const statusSolarType = localStorage.getItem("lastViewedSolar");
-        document.querySelector('.status__row-info').textContent = statusRowNum || 'None';
-        document.querySelector('.status__solar-info').textContent = statusSolarType || 'None';
+        document.querySelector('.status__row-info').textContent = statusRowNum || "None";
+        document.querySelector('.status__solar-info').textContent = statusSolarType || "None";
     };
     
     const updateStatus = event => {
+        const target = event.target;
         let rowNum = localStorage.getItem("lastViewedRow") || NaN;
         let solarType = localStorage.getItem("lastViewedSolar") || '';
+        let query = "";
         
-        if (event.target.nodeName === 'TD') {
-            const rowId = event.target.parentElement.id;
-            solarType = rowId.split('-')[0];
-            rowNum = parseInt(rowId.split('-')[1]) + 1;
-            
-        } else if (event.target.className === 'cells') {
+        if (target.nodeName === "TD" && target.className === "query") {
+            const rowId = target.parentElement.id;
+            solarType = rowId.split("-")[0];
+            rowNum = parseInt(rowId.split("-")[1]) + 1;
+            query = target.textContent;
+        } else if (target.className === "cells") {
             solarType = event.target.textContent.toLowerCase();
         }
-        
+
         localStorage.setItem("lastViewedRow", rowNum);
         localStorage.setItem("lastViewedSolar", solarType);
         displayStatus();
+        
+        if (!!query) {
+            openBrowser(query);
+        }
     };
     
-    document.addEventListener('click', updateStatus);
+    document.addEventListener("click", updateStatus);
     displayStatus();
 })()
