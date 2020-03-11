3b6cde607d2d8b8f43e39f3f4606aced3b87d9e2
Subrata Sarker <subrata_sarker@yahoo.com>
2020-03-11 23:12:37 +0900

    Keep track of page number and row number

--- static/js/script.js
+++ static/js/script.js
@@ -1,55 +1,38 @@
 (() => {
-    
+    var pageNumber = localStorage.getItem("pageNumber") || 1;
+    var rowNumber = localStorage.getItem("rowNumber") || 1;
+
     const openBrowser = (query) => {
         const url = "https://www.google.com/search?q=" + query;
         window.open(url);
     }
-    
-    const displayStatus = () => {
-        const statusSolarType = localStorage.getItem("lastViewedSolar") || "None";
-        let statusRowNum = undefined;
-        
-        if (statusSolarType === 'dsscs') {
-            statusRowNum = localStorage.getItem("lastViewedDSSCs");
-        } else if (statusSolarType === 'prscs') {
-            statusRowNum = localStorage.getItem("lastViewedPrSCs");
-        }
-       
-        document.querySelector(".status__solar-info").textContent = statusSolarType;
-        document.querySelector(".status__row-info").textContent = statusRowNum || "None";
 
+    const displayStatus = () => {
+        document.querySelector(".status__page-info").textContent = pageNumber;
+        document.querySelector(".status__row-info").textContent = rowNumber;
     };
-    
+
     const updateStatus = event => {
         const target = event.target;
-        let solarType = localStorage.getItem("lastViewedSolar") || "";
-        let query = "";
-        
+        let query;
+
         if (target.nodeName === "TD" && target.className === "query") {
             const rowId = target.parentElement.id;
-            const rowNum = parseInt(rowId.split("-")[1]) + 1;
+            pageNumber = parseInt(window.location.href.split("=")[1]) || 1;
+            rowNumber = parseInt(rowId.split("-")[1]) + 1;
             query = target.textContent;
-            solarType = rowId.split("-")[0];
-            
-             if (solarType === 'dsscs') {
-                localStorage.setItem("lastViewedDSSCs", rowNum);
-            } else if (solarType === 'prscs') {
-                localStorage.setItem("lastViewedPrSCs", rowNum);
-            }
-           
-        } else if (target.className === "cells") {
-            solarType = event.target.textContent.toLowerCase();
-        }
 
-        localStorage.setItem("lastViewedSolar", solarType);
-        
+            localStorage.setItem("pageNumber", pageNumber);
+            localStorage.setItem("rowNumber", rowNumber);
+
+        }
         displayStatus();
-        
+
         if (!!query) {
             openBrowser(query);
         }
     };
-    
+
     document.addEventListener("click", updateStatus);
     displayStatus();
 })()
