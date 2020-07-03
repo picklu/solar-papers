fc5c16f47adee9fcc084f41449277732b5e2ad2a
Subrata Sarker <subrata_sarker@yahoo.com>
2020-07-03 14:14:26 +0900

    Use different db for solar

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ DATA = path.join(path.dirname(__file__), "data")
 JSON_PATH = path.join(DATA, "papers.json")
 DB_USER = environ["DB_USER"]
 DB_PASS = environ["DB_PASS"]
-DB_URI = "mongodb://%s:%s@ds035137.mlab.com:35137/solarpapers?retryWrites=false"
+DB_URI = "mongodb://%s:%s@ds263816.mlab.com:63816/ksolarpapers?retryWrites=false"
